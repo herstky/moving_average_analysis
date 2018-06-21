289c6c719212876b84a6b9a793992deca3f03c6e
Kyle <herstky@gmail.com>
2018-06-21 14:44:03 -0700

    implemented axes labels and legend

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    plt.plot(da.day, da.price)
-    plt.plot(da.day[49::], da.maPrice50)
-    plt.plot(da.day[199::], da.maPrice200)
-    
+    plt.plot(da.day, da.price, label = "Prices")
+    plt.plot(da.day[49::], da.maPrice50, label = "50 day MA")
+    plt.plot(da.day[199::], da.maPrice200, label = "200 day MA")
+
+    plt.legend()
+    plt.xlabel("Date")
+    plt.ylabel("Price ($)")
+    plt.title("Procter & Gamble")
     plt.show()
