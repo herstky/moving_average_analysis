bc0489f209374df7957498888dcf51477d4b79ae
Kyle <herstky@gmail.com>
2018-06-22 13:28:43 -0700

    implemented backtesting

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,6 +4,7 @@ import matplotlib.dates as dates
 import numpy as np
 import datetime 
 
+
 class MovingAverage:
     def __init__(self, numDays):
         self.numDays = numDays
@@ -30,13 +31,46 @@ class MovingAverage:
         if day >= self.numDays - 1:
             self.movingAverage.append(self.mean())
             self.dequeue()
-      
+
+class BackTest:
+    def __init__(self):
+        self.bullishDates = []
+        self.bearishDates = []
+        self.tradeList = []
+        self.percentChanges = []
+        self.holding = False
+
+    def movingAverageintercept(self, day, price, MA1, MA2):
+        if MA2.numDays > MA1.numDays:
+            self.smallMA = MA1
+            self.bigMA = MA2
+    
+        else:
+            self.smallMA = MA2
+            self.bigMA = MA1
+        if day >= self.bigMA.numDays:
+            self.curSmallMA = self.smallMA.movingAverage[day - self.smallMA.numDays]
+            self.curBigMA = self.bigMA.movingAverage[day - self.bigMA.numDays]
+            self.prevSmallMA = self.smallMA.movingAverage[day - self.smallMA.numDays - 1]
+            self.prevBigMa = self.bigMA.movingAverage[day - self.bigMA.numDays - 1]
+            if self.curSmallMA > self.curBigMA and self.prevSmallMA < self.prevBigMa:
+                self.bullishDates.append(row['Date'])
+                self.startPrice = price
+                self.holding = True
+            
+            if self.curSmallMA < self.curBigMA and self.prevSmallMA > self.prevBigMa:
+                self.bearishDates.append(row['Date'])    
+                if self.holding == True:
+                    self.endPrice = price     
+                    self.percentChanges.append(np.round(100 * (self.startPrice - self.endPrice) / self.startPrice, 2))
+                    self.holding =False
+
+
 dayList = []
 priceList = []
-
 fiftyDayMA = MovingAverage(50)
 twoHundredDayMA = MovingAverage(200)
-
+test1 = BackTest()
 with open ('prices.csv', 'rt') as csvfile:
     data = DictReader(csvfile)
     day = 0
@@ -48,6 +82,8 @@ with open ('prices.csv', 'rt') as csvfile:
 
         fiftyDayMA.updateMA(day, price)
         twoHundredDayMA.updateMA(day, price)
+
+        test1.movingAverageintercept(day, price, fiftyDayMA, twoHundredDayMA) 
 
         day += 1
 
--- main.py
+++ main.py
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     plt.plot(dayList[fiftyDayMA.numDays - 1::], fiftyDayMA.movingAverage, label = "50 day MA")
     plt.plot(dayList[twoHundredDayMA.numDays - 1::], twoHundredDayMA.movingAverage, label = "200 day MA")
 
+    print('bullish: {}'.format(test1.bullishDates))
+    print('bearish: {}'.format(test1.bearishDates))
+    print('percent changes: {}'.format(test1.percentChanges))
     plt.legend()
     plt.xlabel("Date")
     plt.ylabel("Price ($)")
     plt.title("Procter & Gamble")
     plt.show()
+
+    
